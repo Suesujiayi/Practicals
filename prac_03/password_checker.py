1ff379649afea1b38a91bde4782a9445f8441f7c
Dell <2214612746@qq.com>
2021-09-24 21:17:13 +0800

    delete some function

---
+++ prac_03/password_checker.py
@@ -0,0 +1,26 @@
+MIN_LENGTH = 2
+MAX_LENGTH = 6
+SPECIAL_CHARS_REQUIRED = True
+SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+
+
+def main():
+    """Program to get and check a user's password."""
+    print("Please enter a valid password")
+    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
+          "characters, and contain:")
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
+        password = input("> ")
+    print("Your {}-character password is valid: {}".format(len(password),
+                                                           password))
+
+
+def is_valid_password(password):
+    if len(password) >= MAX_LENGTH or len(password) <= MIN_LENGTH:
+        return False
+
+
+
+main()
