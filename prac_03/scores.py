2a7bf3c03d3075dc652ab7ab01b28df082431bc8
Dell <2214612746@qq.com>
2021-09-27 23:28:10 +0800

    For each of those scores, write the "result" to a file called `results.txt` as below:

--- prac_03/scores.py
+++ prac_03/scores.py
@@ -4,6 +4,10 @@ def main():
     import random
     score = random.randint(0, 100)
     print(score_kinds(score))
+    filename = ' score_kinds.txt  '
+    with open(filename, 'w') as file_object:
+        file_object.write(" {} is {} ".format(score, score_kinds(score)+"\n"))
+        file_object.close()
 
 
 def score_kinds(score):
