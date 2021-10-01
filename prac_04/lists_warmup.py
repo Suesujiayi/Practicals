9a2c3b43bb1b37c8bb7438fd0dc1a0b1d32b3061
Dell <2214612746@qq.com>
2021-10-01 13:49:16 +0800

    Check if 9 is an element of numbers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -8,3 +8,8 @@ print(numbers)
 # Get all the elements from numbers except the first two (slice)
 numbers = numbers[2:7]
 print(numbers)
+# Check if 9 is an element of numbers
+if 9 in numbers:
+    print("9 is an element of numbers")
+else:
+    print("9 is not an element of numbers")
