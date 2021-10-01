294cf8c6797e6aecec671b9882ded2c55f0b7b9f
Dell <2214612746@qq.com>
2021-10-01 17:19:13 +0800

    add the security checker function

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,4 @@
-
-
+# Basic list operations
 def main():
     numbers = []
     for number in range(1, 6):
@@ -13,6 +12,17 @@ def main():
     print("The largest number is ", max(numbers))
     average_of_the_numbers = sum(numbers)/len(numbers)
     print("The average of the numbers is ", average_of_the_numbers)
+
+# Woefully inadequate security checker
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup',
+                 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+                 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+                 'StartServer', 'bob']
+    name = input(" What's your name? ")
+    if name in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
 
 main()
