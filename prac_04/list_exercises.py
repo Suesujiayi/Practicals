9332549099fc3b0329eeeb37628f69ae112c49c0
Dell <2214612746@qq.com>
2021-10-01 17:04:43 +0800

    print the average of the numbers

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -5,11 +5,14 @@ def main():
     for number in range(1, 6):
         number = int(input("Numbers: "))
         numbers.append(number)
-    print(numbers)
     first_number = numbers[0]
     print("The first number is ", first_number)
     last_number = numbers[-1]
     print("The last number is ", last_number)
+    print("The smallest number is ", min(numbers))
+    print("The largest number is ", max(numbers))
+    average_of_the_numbers = sum(numbers)/len(numbers)
+    print("The average of the numbers is ", average_of_the_numbers)
 
 
 main()
