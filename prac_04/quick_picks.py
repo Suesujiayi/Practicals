2f53ae188c36ff7899eb59a7e127af126043e7a7
Dell <2214612746@qq.com>
2021-10-04 18:00:56 +0800

    Add and fix the constants,write a program that asks the user how many "quick picks" they wish to generate.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,12 +1,23 @@
 import random
-CONSTANTS = random.randint(1, 45)
-quick_pick_numbers = int(input("How many 'quick picks' you wish to generate?"))
-numbers=[]
-for j in range(0, quick_pick_numbers):
-    for i in range(0, quick_pick_numbers):
-        numbers.append(CONSTANTS)
-        print(numbers)
+
+THE_NUMBER_PER_LINE = 6
+MAXIMUM = 45
+MINIMUM = 1
 
 
+def main():
+    quick_pick_numbers = int(input("How many 'quick picks' you wish to generate?"))
+    for i in range(quick_pick_numbers):
+        numbers_list = []
+        for j in range(THE_NUMBER_PER_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+            if number in numbers_list:
+                number = random.randint(MINIMUM, MAXIMUM)
+                numbers_list.append(number)
+            else:
+                numbers_list.append(number)
+        numbers_list.sort()
+        print(numbers_list)
 
 
+main()
